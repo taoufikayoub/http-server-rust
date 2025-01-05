77115cad821bb32bfbca9bff881b907dd4f8ecde
ayoub <ayoub@terros.io>
2025-01-05 01:09:24 +0100

    feat: improve TCP connection handling and response

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
-#[allow(unused_imports)]
-use std::net::TcpListener;
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_connection(mut stream: TcpStream) {
+    stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+}
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
